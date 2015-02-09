306fa785533515d26b91f25cf1da32f436eb669e
wcebron <wcebron@gmail.com>
2015-02-09 09:36:46 -0800

    Edited model/added controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,2 @@
 class User < ActiveRecord::Base
-
-	def index
-		@users = User.all
-	end
-
-	def new
-		@user = User.new
-
-	def create
-		@User = User.create(params.require(:user).permit(:username, :email, :dob))
-	end
-
 end
-`
