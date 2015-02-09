6fc44918d2ae5e62e0e7ebebcd42b4bc5f013a71
wcebron <wcebron@gmail.com>
2015-02-09 10:33:37 -0800

    Cleaned up views and recreated database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,23 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.where(id: params[:id]).first #use this instead of find so you don't get red screen if it doesn't find anything
   end
 
   def new
     @user = User.new
   end
 
-  def edit
+  def create
+    @user = User.create(params.require(:user).permit(:username, :email, :dob))
+    if @user.save
+      redirect_to users_path
+    else
+      render 'new'
+    end
   end
 
-  def create
+  def edit
   end
 
   def update
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,9 @@
 ActiveRecord::Schema.define(version: 20150209170432) do
 
   create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.date     "dob"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
